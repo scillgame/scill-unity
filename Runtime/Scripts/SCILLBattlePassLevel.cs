38f8301963e832d44f5c0af33f9b3fd9e5b4f12c
Phillip Schuster <pschuster@me.com>
2021-01-12 08:17:41 +0100

    Added outline for selected level and reworked selection management

--- Runtime/Scripts/SCILLBattlePassLevel.cs
+++ Runtime/Scripts/SCILLBattlePassLevel.cs
@@ -16,12 +16,20 @@ public class SCILLBattlePassLevel : MonoBehaviour
     public GameObject battlePassLevelInfo;
     [Tooltip("A textfield that will be used to render the level number")]
     public Text levelName;
+    [Tooltip("Set the name of the level, use {level} for the level number")]
+    public string levelTextFormat = "Level {level}";
 
-    [Header("Prefabs")] [Tooltip("A prefab that will be used as a reward icon")]
+    [Header("Prefabs")] 
+    [Tooltip("A prefab that will be used as a reward icon")]
     public SCILLBattlePassRewardIcon rewardIconPrefab;
     
     [Tooltip("A slider that will be used to render the current progress in this level")]
     public Slider progressSlider;
+    [Tooltip("Automatically disable progress slider if value is 0 or 1.")]
+    public bool autoHideProgressSlider = false;
+
+    [Tooltip("A text ui object that will be set with a string like 2/4 - which is 2 of 4 challenges completed.")]
+    public Text challengeText;
 
     [HideInInspector]
     public bool showLevelInfo = true;
@@ -56,12 +64,33 @@ public class SCILLBattlePassLevel : MonoBehaviour
     private void OnEnable()
     {
         SCILLBattlePassManager.OnBattlePassChallengeUpdate += OnBattlePassChallengeUpdate;
+        SCILLBattlePassManager.OnSelectedBattlePassLevelChanged += OnSelectedBattlePassLevelChanged;
         UpdateUI();
+    }
+
+    private void OnSelectedBattlePassLevelChanged(BattlePassLevel selectedBattlePassLevel)
+    {
+        if (battlePassLevel == null || selectedBattlePassLevel == null)
+        {
+            Deselect();
+        }
+        else
+        {
+            if (battlePassLevel.level_id == selectedBattlePassLevel.level_id)
+            {
+                Select();
+            }
+            else
+            {
+                Deselect();
+            }            
+        }
     }
 
     private void OnDestroy()
     {
         SCILLBattlePassManager.OnBattlePassChallengeUpdate -= OnBattlePassChallengeUpdate;
+        SCILLBattlePassManager.OnSelectedBattlePassLevelChanged -= OnSelectedBattlePassLevelChanged;
     }
 
     private void OnBattlePassChallengeUpdate(BattlePassChallengeChangedPayload challengeChangedPayload)
@@ -95,44 +124,81 @@ public class SCILLBattlePassLevel : MonoBehaviour
             }
         }
 
-        levelName.text = battlePassLevel.level_priority.ToString();
-        battlePassLevelInfo.SetActive(showLevelInfo);
+        if (levelName)
+        {
+            levelName.text = levelTextFormat.Replace("{level}", battlePassLevel.level_priority.ToString());    
+        }
 
+        if (battlePassLevelInfo) battlePassLevelInfo.SetActive(showLevelInfo);
+
+        float totalProgress = 0;
+        int activatedChallenges = 0; 
+        foreach (var challenge in battlePassLevel.challenges)
+        {
+            float challengeProgress = 0;
+            if (challenge.challenge_goal > 0)
+            {
+                challengeProgress = (float) challenge.user_challenge_current_score /
+                                    (float) challenge.challenge_goal;
+            }
+
+            totalProgress += challengeProgress * (1.0f / (float)battlePassLevel.challenges.Count);
+
+            if (challenge.user_challenge_current_score >= challenge.challenge_goal)
+            {
+                activatedChallenges += 1;
+            }
+        }
+        
         // Update slider
         if (progressSlider)
         {
-            float totalProgress = 0;
-            foreach (var challenge in battlePassLevel.challenges)
-            {
-                float challengeProgress = 0;
-                if (challenge.challenge_goal > 0)
-                {
-                    challengeProgress = (float) challenge.user_challenge_current_score /
-                                        (float) challenge.challenge_goal;
-                }
+            progressSlider.value = totalProgress;
 
-                totalProgress += challengeProgress * (1.0f / (float)battlePassLevel.challenges.Count);
+            if (autoHideProgressSlider)
+            {
+                if (totalProgress <= 0)
+                {
+                    progressSlider.gameObject.SetActive(false);
+                }
+                else
+                {
+                    progressSlider.gameObject.SetActive(true);
+                }                
             }
-            
-            if (totalProgress <= 0)
+
+            if (battlePassLevel.level_priority <= 1)
             {
                 progressSlider.gameObject.SetActive(false);
             }
-            else
-            {
-                progressSlider.value = totalProgress;
-                progressSlider.gameObject.SetActive(true);
-            }
+        }
+
+        if (challengeText)
+        {
+            challengeText.text = activatedChallenges + "/" + battlePassLevel.challenges.Count;
+        }
+
+        if (SCILLBattlePassManager.Instance.SelectedBattlePassLevel?.battle_pass_id == battlePassLevel.level_id)
+        {
+            Select();
         }
     }
 
     public virtual void Select()
     {
-        
+        var outline = GetComponentInChildren<Outline>();
+        if (outline)
+        {
+            outline.enabled = true;
+        }
     }
 
     public virtual void Deselect()
     {
-        
+        var outline = GetComponentInChildren<Outline>();
+        if (outline)
+        {
+            outline.enabled = false;
+        }
     }
 }
